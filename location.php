02f082e1f5c86b25c351251b0cad452a32068495
nhjung@enliple.com <nhjung@enliple.com>
2017-06-02 11:14:33 +0900

    tqweqweqwe

--- location.php
+++
@@ -1,2 +0,0 @@
-<?php
-header("Location:http://naver.com'");
---
+++ t.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+<div class="test">
+ddd
+    <?php
+        href.location(http://www.naver.com);
+
+    ?>
+<?
+echo "<meta http-equiv='refresh' content='0; url=http://seobangnim.com'>";
+?>
+</div>
+</body>
+</html>
+
